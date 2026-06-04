9cc72fd52014c9c55a68dc8aa50c2cee769bac45
tadjo <djordjevuckovicc@gmail.com>
2026-06-05 00:21:27 +0200

    style(db): trim migration comment

--- db/migrations/006_add_trgm_indexes.up.sql
+++ db/migrations/006_add_trgm_indexes.up.sql
@@ -1,7 +1,5 @@
 BEGIN;
--- GIN trigram indexes backing the news_fuzzy pg-trgm engine (similarity() queries).
--- idx_articles_title_trgm backs the pg_trgm_title template.
--- idx_articles_title_desc_trgm backs the pg_trgm_multi template (title + description).
+-- GIN trigram indexes backing the news_fuzzy pg-trgm engine.
 CREATE INDEX IF NOT EXISTS idx_articles_title_trgm ON articles
     USING gin (title gin_trgm_ops);
 CREATE INDEX IF NOT EXISTS idx_articles_title_desc_trgm ON articles
