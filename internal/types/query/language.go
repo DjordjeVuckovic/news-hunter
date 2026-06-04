9af34ea126426b1163ce3234a0f22f46c7da03d1
tadjo <djordjevuckovicc@gmail.com>
2026-06-04 23:49:03 +0200

    refactor(query): rename LanguageSpanish to LanguageSerbian

--- internal/types/query/language.go
+++ internal/types/query/language.go
@@ -6,14 +6,14 @@ type Language string
 
 const (
 	LanguageEnglish Language = "english"
-	LanguageSpanish Language = "serbian"
+	LanguageSerbian Language = "serbian"
 )
 
 var DefaultLanguage = LanguageEnglish
 
 var SupportedLanguages = map[Language]bool{
 	LanguageEnglish: true,
-	LanguageSpanish: true,
+	LanguageSerbian: true,
 }
 
 func (l Language) Parse() (Language, error) {
